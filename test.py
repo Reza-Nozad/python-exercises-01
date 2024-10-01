f454f51e11c222aa0556e6fec12c1f5a86135c71
reza <reza.nozad@gmail.com>
2024-10-01 13:36:41 +0200

    Python Datei ist geändert

---
+++ test.py
@@ -0,0 +1,21 @@
+first_name="Reza"
+last_name="Nozad"
+birth_year="1988"
+has_license=True
+
+print("mein Name ist:", first_name)
+print("mein Lastname ist:", last_name)
+print("mein Geburtsjahr ist:", birth_year)
+print("mein Führerscheinstatus:", has_license)
+
+
+
+
+
+
+
+
+
+
+
+

220fb9cc7ee7ebb434df6d470574817224f8b83c
reza <reza.nozad@gmail.com>
2024-10-01 14:34:11 +0200

    Aufgabe 2 ist erledigt

--- test.py
+++ test.py
@@ -12,11 +12,12 @@ print("mein Führerscheinstatus:", has_license)
 a=10
 b=3
 print("Die Summe von a und b =", a+b)
-print("Die Differenz von a und b", a-b)
-print("Den ganzzahligen Quotienten von a und b", a//b)
-print(" Das Produkt von a und b", a*b)
-print(" Den Rest der Division von a und b", a%b)
-print("Den Wert von a hoch b", a**b)
+print("Die Differenz von a und b =", a-b)
+print("Den ganzzahligen Quotienten von a und b =", a//b)
+print(" Das Produkt von a und b =", a*b)
+print(" Den Rest der Division von a und b =", a%b)
+print("Den Wert von a hoch b =", a**b)
+
 
 
 
